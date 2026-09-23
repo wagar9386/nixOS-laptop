54790417a12486740022f54a1c9a6be87d97d23d
wagar9386 <arnau.goti@gmail.com>
2026-09-23 13:23:45 +0200

    letest

--- Packages/packettracer.nix
+++ Packages/packettracer.nix
@@ -1,12 +1,27 @@
 { pkgs, lib }:
 
+let
+  packetTracerDeb = pkgs.requireFile {
+    name = "CiscoPacketTracer_900_Ubuntu_64bit.deb";
+    sha256 = "086r5qbvvf8qarp554j4q4044vgswv3xfzv2iyvdqdzwqzac16nx";
+    message = ''
+      Cisco Packet Tracer 9.0.0 is required.
+
+      Download:
+      CiscoPacketTracer_900_Ubuntu_64bit.deb
+
+      Then run:
+
+        nix-prefetch-url --type sha256 file:///home/agar/Downloads/CiscoPacketTracer_900_Ubuntu_64bit.deb
+    '';
+  };
+in
+
 pkgs.stdenv.mkDerivation {
   pname = "cisco-packet-tracer";
   version = "9.0.0";
 
-
-  src = /home/agar/Downloads/CiscoPacketTracer_900_Ubuntu_64bit.deb;
-
+  src = packetTracerDeb;
 
   nativeBuildInputs = [
     pkgs.dpkg
