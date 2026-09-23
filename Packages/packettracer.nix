355f4405276e051dce61949cf9342e7d8cb18292
wagar9386 <arnau.goti@gmail.com>
2026-09-23 13:26:23 +0200

    letest2

--- Packages/packettracer.nix
+++ Packages/packettracer.nix
@@ -1,45 +1,52 @@
-{ pkgs, lib }:
+```nix
+  { pkgs, lib }:
 
-let
-  packetTracerDeb = pkgs.requireFile {
-    name = "CiscoPacketTracer_900_Ubuntu_64bit.deb";
-    sha256 = "086r5qbvvf8qarp554j4q4044vgswv3xfzv2iyvdqdzwqzac16nx";
-    message = ''
-      Cisco Packet Tracer 9.0.0 is required.
+  let
+    packetTracerDeb = pkgs.requireFile {
+      name = "CiscoPacketTracer_900_Ubuntu_64bit.deb";
+      sha256 = "086r5qbvvf8qarp554j4q4044vgswv3xfzv2iyvdqdzwqzac16nx";
+      message = ''
+        Cisco Packet Tracer 9.0.0 is required.
 
-      Download:
-      CiscoPacketTracer_900_Ubuntu_64bit.deb
+        Download the .deb and add it to the Nix store with:
 
-      Then run:
+          nix-store --add-fixed sha256 ~/Downloads/CiscoPacketTracer_900_Ubuntu_64bit.deb
+      '';
+    };
+  in
 
-        nix-prefetch-url --type sha256 file:///home/agar/Downloads/CiscoPacketTracer_900_Ubuntu_64bit.deb
+  pkgs.stdenv.mkDerivation {
+    pname = "cisco-packet-tracer";
+    version = "9.0.0";
+
+    src = packetTracerDeb;
+
+    nativeBuildInputs = [
+      pkgs.dpkg
+      pkgs.makeWrapper
+    ];
+
+    unpackPhase = ''
+      dpkg-deb -x $src .
     '';
-  };
-in
 
-pkgs.stdenv.mkDerivation {
-  pname = "cisco-packet-tracer";
-  version = "9.0.0";
+    installPhase = ''
+      mkdir -p $out
+      cp -r ./* $out/
 
-  src = packetTracerDeb;
+      mkdir -p $out/bin
 
-  nativeBuildInputs = [
-    pkgs.dpkg
-  ];
+      makeWrapper \
+        $out/opt/pt/packettracer \
+        $out/bin/packettracer
+    '';
 
-  unpackPhase = ''
-    dpkg-deb -x $src .
-  '';
+    meta = {
+      description = "Cisco Packet Tracer";
+      homepage = "https://www.netacad.com/";
+      license = lib.licenses.unfree;
+      platforms = [ "x86_64-linux" ];
+    };
+  }
+```
 
-  installPhase = ''
-    mkdir -p $out
-    cp -r ./* $out/
-  '';
-
-  meta = {
-    description = "Cisco Packet Tracer";
-    homepage = "https://www.netacad.com/";
-    license = lib.licenses.unfree;
-    platforms = [ "x86_64-linux" ];
-  };
-}
